59524ec50e53b0b459033b084ad0c41b40532794
xujianglong <jianglong.xu@tiho-mobi.com>
2024-03-18 15:49:29 +0800

    继续本地化GameActivity的代码

--- app/src/main/java/com/muen/flappybird/ui/GameActivity.kt
+++ app/src/main/java/com/muen/flappybird/ui/GameActivity.kt
@@ -1,12 +1,12 @@
 package com.muen.flappybird.ui
 
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Handler
 import android.os.Looper
 import android.view.MotionEvent
-import android.view.View
 import com.muen.flappybird.R
 import com.muen.flappybird.databinding.ActivityGameBinding
 import com.muen.flappybird.util.BaseActivity
@@ -17,14 +17,14 @@ class GameActivity : BaseActivity<ActivityGameBinding>() {
     //位置
     private var birdX= 0.0f
     private var birdY= 0.0f
-    private var pipeUstX = 0.0f
-    private var pipeUstY = 0.0f
-    private var pipeAltX = 0.0f
-    private var pipeAltY = 0.0f
-    private var pipe2UstX = 0.0f
-    private var pipe2UstY = 0.0f
-    private var pipe2AltX = 0.0f
-    private var pipe2AltY = 0.0f
+    private var pipeTopLargeX = 0.0f
+    private var pipeTopLargeY = 0.0f
+    private var pipeBottomLargeX = 0.0f
+    private var pipeBottomLargeY = 0.0f
+    private var pipeTopSmallX = 0.0f
+    private var pipeTopSmallY = 0.0f
+    private var pipeBottomSmallX = 0.0f
+    private var pipeBottomSmallY = 0.0f
 
     //控制器
     private var touchControl = false
@@ -48,41 +48,37 @@ class GameActivity : BaseActivity<ActivityGameBinding>() {
         return ActivityGameBinding.inflate(layoutInflater)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun initListener() {
         super.initListener()
 
-        viewBinding.cl.setOnTouchListener(object : View.OnTouchListener{
-            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        viewBinding.view.setOnTouchListener { _, event ->
+            if (startControl) {
+                if (event?.action == MotionEvent.ACTION_DOWN) {
+                    birdFlyAnimator()
+                    touchControl = true
+                }
+                if (event?.action == MotionEvent.ACTION_UP) {
+                    touchControl = false
+                }
+            } else {
+                startControl = true
+                removeTips()
+                updateLocation()
 
-                if (startControl){
-                    if(event?.action == MotionEvent.ACTION_DOWN){
-                        birdFlyAnimator()
-                        touchControl = true
-                    }
-                    if (event?.action == MotionEvent.ACTION_UP){
-                        touchControl = false
-                    }
-                } else{
-                    startControl = true
-                    removeTips()
-                    updateLocation()
-
-                    timer.schedule(0,30){
-                        Handler(Looper.getMainLooper()).post{
-                            birdMove()
-                            pipeMove()
-                            collide()
-                        }
+                timer.schedule(0, 30) {
+                    Handler(Looper.getMainLooper()).post {
+                        birdMove()
+                        pipeMove()
+                        collide()
                     }
                 }
-
-
-                return true
             }
-        })
+            true
+        }
     }
 
-    fun birdMove(){
+    private fun birdMove(){
         if (touchControl){
             touchControl=false
             birdY-=birdHeight+birdHeight/5
@@ -96,123 +92,123 @@ class GameActivity : BaseActivity<ActivityGameBinding>() {
         if(birdY >= screenHeight-birdHeight){
             birdY =(screenHeight-birdHeight).toFloat()
         }
-        viewBinding.imgfvBird.y=birdY
+        viewBinding.bird.y=birdY
     }
 
-    fun birdFlyAnimator(){
-        val flyAnimator = ObjectAnimator.ofFloat(viewBinding.imgfvBird,"rotation",-25.0f,0.0f).apply {
+    private fun birdFlyAnimator(){
+        val flyAnimator = ObjectAnimator.ofFloat(viewBinding.bird,"rotation",-25.0f,0.0f).apply {
         }
         flyAnimator.start()
     }
 
-    fun removeTips(){
+    private fun removeTips(){
         val alphaAnimator = ObjectAnimator.ofFloat(viewBinding.txtTips,"alpha",1.0f,0.0f).apply {
             duration = 500
         }
         alphaAnimator.start()
     }
 
-    fun updateLocation(){
-        birdX = viewBinding.imgfvBird.x
-        birdY = viewBinding.imgfvBird.y
+    private fun updateLocation(){
+        birdX = viewBinding.bird.x
+        birdY = viewBinding.bird.y
 
-        pipeUstX = viewBinding.imgfvPipeTopL.x
-        pipeUstY = viewBinding.imgfvPipeTopL.y
-        pipeAltX = viewBinding.imgfvPipeBottomL.x
-        pipeAltY = viewBinding.imgfvPipeBottomL.y
-        pipe2AltX = viewBinding.imgfvPipeBottomS.x
-        pipe2AltY = viewBinding.imgfvPipeBottomS.y
-        pipe2UstX = viewBinding.imgfvPipeTopS.x
-        pipe2UstY = viewBinding.imgfvPipeTopS.y
+        pipeTopLargeX = viewBinding.pipeTopLarge.x
+        pipeTopLargeY = viewBinding.pipeTopLarge.y
+        pipeBottomLargeX = viewBinding.pipeBottomLarge.x
+        pipeBottomLargeY = viewBinding.pipeBottomLarge.y
+        pipeBottomSmallX = viewBinding.pipeBottomSmall.x
+        pipeBottomSmallY = viewBinding.pipeBottomSmall.y
+        pipeTopSmallX = viewBinding.pipeTopSmall.x
+        pipeTopSmallY = viewBinding.pipeTopSmall.y
 
 
-        screenHeight = viewBinding.cl.height
-        screenWidth = viewBinding.cl.width
-        birdHeight = viewBinding.imgfvBird.height
-        birdWidth = viewBinding.imgfvBird.width
+        screenHeight = viewBinding.view.height
+        screenWidth = viewBinding.view.width
+        birdHeight = viewBinding.bird.height
+        birdWidth = viewBinding.bird.width
 
-        pipeTopHeight = viewBinding.imgfvPipeTopL.height
-        pipeTopWidth = viewBinding.imgfvPipeTopL.width
+        pipeTopHeight = viewBinding.pipeTopLarge.height
+        pipeTopWidth = viewBinding.pipeTopLarge.width
     }
 
-    fun pipeMove(){
+    private fun pipeMove(){
 
-        pipeUstX-=15.0f
-        pipeAltX-=15.0f
+        pipeTopLargeX-=15.0f
+        pipeBottomLargeX-=15.0f
 
-        if (pipeUstX < -pipeTopWidth && pipeAltX < -pipeTopWidth){
-            pipeUstX = (screenWidth +pipeTopWidth).toFloat()
-            pipeAltX = (screenWidth +pipeTopWidth).toFloat()
+        if (pipeTopLargeX < -pipeTopWidth && pipeBottomLargeX < -pipeTopWidth){
+            pipeTopLargeX = (screenWidth +pipeTopWidth).toFloat()
+            pipeBottomLargeX = (screenWidth +pipeTopWidth).toFloat()
 
             val cikacakSayi = screenHeight - pipeTopHeight
 
-            pipeUstY = ((birdHeight..screenHeight-cikacakSayi).random()-pipeTopHeight).toFloat()
-            pipeAltY = (pipeUstY+pipeTopHeight+(birdHeight*3))
+            pipeTopLargeY = ((birdHeight..screenHeight-cikacakSayi).random()-pipeTopHeight).toFloat()
+            pipeBottomLargeY = (pipeTopLargeY+pipeTopHeight+(birdHeight*3))
         }
 
-        viewBinding.imgfvPipeTopL.x = pipeUstX
-        viewBinding.imgfvPipeBottomL.x = pipeAltX
+        viewBinding.pipeTopLarge.x = pipeTopLargeX
+        viewBinding.pipeBottomLarge.x = pipeBottomLargeX
 
-        viewBinding.imgfvPipeTopL.y = pipeUstY
-        viewBinding.imgfvPipeBottomL.y = pipeAltY
+        viewBinding.pipeTopLarge.y = pipeTopLargeY
+        viewBinding.pipeBottomLarge.y = pipeBottomLargeY
 
 
-        pipe2UstX-=15.0f
-        pipe2AltX-=15.0f
+        pipeTopSmallX-=15.0f
+        pipeBottomSmallX-=15.0f
 
-        if(pipe2UstX < -pipeTopWidth && pipe2AltX < -pipeTopWidth){
-            pipe2UstX = (screenWidth +pipeTopWidth).toFloat()
-            pipe2AltX = (screenWidth +pipeTopWidth).toFloat()
+        if(pipeTopSmallX < -pipeTopWidth && pipeBottomSmallX < -pipeTopWidth){
+            pipeTopSmallX = (screenWidth +pipeTopWidth).toFloat()
+            pipeBottomSmallX = (screenWidth +pipeTopWidth).toFloat()
 
             val cikacakSayi = screenHeight - pipeTopHeight
 
-            pipe2UstY = ((birdHeight..screenHeight-cikacakSayi).random()-pipeTopHeight).toFloat()
-            pipe2AltY = (pipe2UstY+pipeTopHeight+(birdHeight*3))
+            pipeTopSmallY = ((birdHeight..screenHeight-cikacakSayi).random()-pipeTopHeight).toFloat()
+            pipeBottomSmallY = (pipeTopSmallY+pipeTopHeight+(birdHeight*3))
         }
 
-        viewBinding.imgfvPipeTopS.x=pipe2UstX
-        viewBinding.imgfvPipeBottomS.x=pipe2AltX
+        viewBinding.pipeTopSmall.x=pipeTopSmallX
+        viewBinding.pipeBottomSmall.x=pipeBottomSmallX
 
-        viewBinding.imgfvPipeTopS.y = pipe2UstY
-        viewBinding.imgfvPipeBottomS.y = pipe2AltY
+        viewBinding.pipeTopSmall.y = pipeTopSmallY
+        viewBinding.pipeBottomSmall.y = pipeBottomSmallY
     }
 
-    fun collide(){
-        var bSagUx = viewBinding.imgfvBird.x+birdWidth
-        var bSagUy = viewBinding.imgfvBird.y
-        var bSolUx = viewBinding.imgfvBird.x
-        var bSolUy = viewBinding.imgfvBird.y
+    private fun collide(){
+        var bSagUx = viewBinding.bird.x+birdWidth
+        var bSagUy = viewBinding.bird.y
+        var bSolUx = viewBinding.bird.x
+        var bSolUy = viewBinding.bird.y
 
-        var bSagAx = viewBinding.imgfvBird.x+birdWidth
-        var bSagAy = viewBinding.imgfvBird.y + birdHeight
-        var bSolAx = viewBinding.imgfvBird.x
-        var bSolAy = viewBinding.imgfvBird.y + birdHeight
+        var bSagAx = viewBinding.bird.x+birdWidth
+        var bSagAy = viewBinding.bird.y + birdHeight
+        var bSolAx = viewBinding.bird.x
+        var bSolAy = viewBinding.bird.y + birdHeight
 
-        var pipeMidpoint = viewBinding.imgfvPipeTopL.x + viewBinding.imgfvPipeTopL.width/2
-        var pipe2Midpoint = viewBinding.imgfvPipeTopS.x + viewBinding.imgfvPipeTopS.width/2
-        var birdMidpoint = viewBinding.imgfvBird.x + viewBinding.imgfvBird.width/2
+        var pipeMidpoint = viewBinding.pipeTopLarge.x + viewBinding.pipeTopLarge.width/2
+        var pipe2Midpoint = viewBinding.pipeTopSmall.x + viewBinding.pipeTopSmall.width/2
+        var birdMidpoint = viewBinding.bird.x + viewBinding.bird.width/2
 
 
 
-        if (bSagUy >= viewBinding.imgfvPipeTopL.y && bSagUy <= viewBinding.imgfvPipeTopL.y + pipeTopHeight &&
-            bSolUy >= viewBinding.imgfvPipeTopL.y && bSolUy <= viewBinding.imgfvPipeTopL.y + pipeTopHeight &&
-            bSagUx >= viewBinding.imgfvPipeTopL.x && bSagUx <= viewBinding.imgfvPipeTopL.x + pipeTopWidth &&
-            bSolUx >= viewBinding.imgfvPipeTopL.x && bSolUx <= viewBinding.imgfvPipeTopL.x + pipeTopWidth
+        if (bSagUy >= viewBinding.pipeTopLarge.y && bSagUy <= viewBinding.pipeTopLarge.y + pipeTopHeight &&
+            bSolUy >= viewBinding.pipeTopLarge.y && bSolUy <= viewBinding.pipeTopLarge.y + pipeTopHeight &&
+            bSagUx >= viewBinding.pipeTopLarge.x && bSagUx <= viewBinding.pipeTopLarge.x + pipeTopWidth &&
+            bSolUx >= viewBinding.pipeTopLarge.x && bSolUx <= viewBinding.pipeTopLarge.x + pipeTopWidth
             ||
-            bSagUy >= viewBinding.imgfvPipeTopS.y && bSagUy <= viewBinding.imgfvPipeTopS.y + pipeTopHeight &&
-            bSolUy >= viewBinding.imgfvPipeTopS.y && bSolUy <= viewBinding.imgfvPipeTopS.y + pipeTopHeight &&
-            bSagUx >= viewBinding.imgfvPipeTopS.x && bSagUx <= viewBinding.imgfvPipeTopS.x + pipeTopWidth &&
-            bSolUx >= viewBinding.imgfvPipeTopS.x && bSolUx <= viewBinding.imgfvPipeTopS.x + pipeTopWidth
+            bSagUy >= viewBinding.pipeTopSmall.y && bSagUy <= viewBinding.pipeTopSmall.y + pipeTopHeight &&
+            bSolUy >= viewBinding.pipeTopSmall.y && bSolUy <= viewBinding.pipeTopSmall.y + pipeTopHeight &&
+            bSagUx >= viewBinding.pipeTopSmall.x && bSagUx <= viewBinding.pipeTopSmall.x + pipeTopWidth &&
+            bSolUx >= viewBinding.pipeTopSmall.x && bSolUx <= viewBinding.pipeTopSmall.x + pipeTopWidth
             ||
-            bSagAy >= viewBinding.imgfvPipeBottomL.y && bSagAy <= viewBinding.imgfvPipeBottomL.y + pipeTopHeight &&
-            bSolAy >= viewBinding.imgfvPipeBottomL.y && bSolAy <= viewBinding.imgfvPipeBottomL.y + pipeTopHeight &&
-            bSagAx >= viewBinding.imgfvPipeBottomL.x && bSagAx <= viewBinding.imgfvPipeBottomL.x + pipeTopWidth &&
-            bSolAx >= viewBinding.imgfvPipeBottomL.x && bSolAx <= viewBinding.imgfvPipeBottomL.x + pipeTopWidth
+            bSagAy >= viewBinding.pipeBottomLarge.y && bSagAy <= viewBinding.pipeBottomLarge.y + pipeTopHeight &&
+            bSolAy >= viewBinding.pipeBottomLarge.y && bSolAy <= viewBinding.pipeBottomLarge.y + pipeTopHeight &&
+            bSagAx >= viewBinding.pipeBottomLarge.x && bSagAx <= viewBinding.pipeBottomLarge.x + pipeTopWidth &&
+            bSolAx >= viewBinding.pipeBottomLarge.x && bSolAx <= viewBinding.pipeBottomLarge.x + pipeTopWidth
             ||
-            bSagAy >= viewBinding.imgfvPipeBottomS.y && bSagAy <= viewBinding.imgfvPipeBottomS.y + pipeTopHeight &&
-            bSolAy >= viewBinding.imgfvPipeBottomS.y && bSolAy <= viewBinding.imgfvPipeBottomS.y + pipeTopHeight &&
-            bSagAx >= viewBinding.imgfvPipeBottomS.x && bSagAx <= viewBinding.imgfvPipeBottomS.x + pipeTopWidth &&
-            bSolAx >= viewBinding.imgfvPipeBottomS.x && bSolAx <= viewBinding.imgfvPipeBottomS.x + pipeTopWidth){
+            bSagAy >= viewBinding.pipeBottomSmall.y && bSagAy <= viewBinding.pipeBottomSmall.y + pipeTopHeight &&
+            bSolAy >= viewBinding.pipeBottomSmall.y && bSolAy <= viewBinding.pipeBottomSmall.y + pipeTopHeight &&
+            bSagAx >= viewBinding.pipeBottomSmall.x && bSagAx <= viewBinding.pipeBottomSmall.x + pipeTopWidth &&
+            bSolAx >= viewBinding.pipeBottomSmall.x && bSolAx <= viewBinding.pipeBottomSmall.x + pipeTopWidth){
 
             timer.cancel()
             mediaPlayer = MediaPlayer.create(this, R.raw.carpma)
